ad26fac843d84e06497fa0bbefee5bdd63a5564d
Marcos Lopes <marcos@dourasoft.com.br>
2020-09-12 23:27:25 -0400

    Finalizando visual

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$('a').click(function() {
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+
+    return false;
+});
+
+$(window).scroll(function(){
+    const bannerMenu = $('#banner-menu'),
+    home = $('#abrir-home'),
+    scroll = $(window).scrollTop();
+  
+    if (scroll >= 100){
+        bannerMenu.addClass('fixed');
+        home.show();
+    }else{
+        bannerMenu.removeClass('fixed');
+        home.hide();
+    } 
+});
+
+$("#menu-mobile").click(function() {
+    $("#abrir-menu-mobile").slideToggle();
+});
+
+const options = {
+    strings: [
+        'Desenvolvimento <b class="titulo-port-main">WEB</b>',
+        'Desenvolvimento <b class="titulo-port-main">MOBILE</b>',
+        'Desenvolvimento <b class="titulo-port-main">DESKTOP</b>',
+        'Desenvolvedor <b class="titulo-port-main">WEB | MOBILE | DESKTOP</b>'
+    ],
+    typeSpeed: 80,
+};
+
+const typed = new Typed('#digitar', options);
